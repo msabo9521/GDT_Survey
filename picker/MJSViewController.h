621c30cbf338bdc84a68275d27544b3e696ce2e2
Michael Sabo <msabo@Michaels-MacBook-Pro-2.local>
2013-10-28 21:24:25 -0500

    3 working buttons

--- picker/MJSViewController.h
+++ picker/MJSViewController.h
@@ -8,6 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MJSViewController : UIViewController
+@interface MJSViewController : UIViewController<UIPopoverControllerDelegate, UIPickerViewDataSource,UIPickerViewDelegate, UITextFieldDelegate>
+
+
+@property (strong, nonatomic) IBOutlet UILabel *button1;
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UIButton *button2;
+@property (strong, nonatomic) IBOutlet UILabel *label2;
+@property (strong, nonatomic) IBOutlet UILabel *label3;
+@property (strong, nonatomic) IBOutlet UITextField *textField3;
+
+
+
+
+- (IBAction)button1Press:(id)sender;
+- (IBAction)button2Press:(id)sender;
+- (IBAction)button3:(id)sender;
+- (IBAction)textField3Done:(id)sender;
+
 
 @end
--- picker/MJSViewController.m
+++ picker/MJSViewController.m
@@ -9,6 +9,17 @@
 #import "MJSViewController.h"
 
 @interface MJSViewController ()
+{
+    UIPopoverController *popOverController;
+    UIPickerView *picker;
+    UIToolbar *pickerToolBar;
+    NSMutableArray *arrayBuilder;
+    NSInteger buttonIndex;
+   
+    
+    
+    
+}
 
 @end
 
@@ -18,6 +29,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _textField3.delegate = self;
+    arrayBuilder =[[NSMutableArray alloc]initWithObjects:@"Rose", @"1MC", @"CPB", @"ACB", nil];
+    _textField3.hidden = YES;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +41,126 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)button1Press:(id)sender {
+    buttonIndex = 1;
+    arrayBuilder =[[NSMutableArray alloc]initWithObjects:@"Rose", @"1MC", @"CPB", @"ACB", nil];
+    picker = [[UIPickerView alloc]init];
+    pickerToolBar = [[UIToolbar alloc]init];
+    
+    UIViewController *popOverContent = [[UIViewController alloc]init];
+    UIView *popOverView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, [picker frame].size.width, [picker frame].size.height+44)];
+    picker = [[UIPickerView alloc]initWithFrame:CGRectMake([popOverView frame].origin.x, [popOverView frame].origin.y + 44, 0, 0)];
+    pickerToolBar = [[UIToolbar alloc]initWithFrame:CGRectMake([popOverView frame].origin.x, [popOverView frame].origin.y, [popOverView frame].size.width, 44)];
+    [picker setDataSource:self];
+    [picker setDelegate:self];
+    [picker setShowsSelectionIndicator:YES];
+    
+    [pickerToolBar setBarStyle:UIBarStyleBlack];
+    UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:Nil action:nil];
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc]initWithTitle:@"Done" style:UIBarButtonItemStyleBordered target:self action:@selector(donePressed)];
+    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc]initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelPressed)];
+    [pickerToolBar setItems:[NSArray arrayWithObjects:flexSpace,doneButton,cancelButton, nil]];
+    
+    [popOverView addSubview:picker];
+    [popOverView addSubview:pickerToolBar];
+    [popOverContent setView:popOverView];
+    
+    popOverController = [[UIPopoverController alloc]initWithContentViewController:popOverContent];
+    popOverController.popoverContentSize = CGSizeMake([picker frame].size.width, [popOverView frame].size.height);
+    [popOverController presentPopoverFromRect:[self button1].frame inView:[self view] permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+}
+
+- (IBAction)button2Press:(id)sender {
+    buttonIndex = 2;
+    arrayBuilder =[[NSMutableArray alloc]initWithObjects:@"1", @"2", @"3", @"4", nil];
+    picker = [[UIPickerView alloc]init];
+    pickerToolBar = [[UIToolbar alloc]init];
+    
+    UIViewController *popOverContent = [[UIViewController alloc]init];
+    UIView *popOverView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, [picker frame].size.width, [picker frame].size.height+44)];
+    picker = [[UIPickerView alloc]initWithFrame:CGRectMake([popOverView frame].origin.x, [popOverView frame].origin.y + 44, 0, 0)];
+    pickerToolBar = [[UIToolbar alloc]initWithFrame:CGRectMake([popOverView frame].origin.x, [popOverView frame].origin.y, [popOverView frame].size.width, 44)];
+    [picker setDataSource:self];
+    [picker setDelegate:self];
+    [picker setShowsSelectionIndicator:YES];
+    
+    [pickerToolBar setBarStyle:UIBarStyleBlack];
+    UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:Nil action:nil];
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc]initWithTitle:@"Done" style:UIBarButtonItemStyleBordered target:self action:@selector(donePressed)];
+    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc]initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelPressed)];
+    [pickerToolBar setItems:[NSArray arrayWithObjects:flexSpace,doneButton,cancelButton, nil]];
+    
+    [popOverView addSubview:picker];
+    [popOverView addSubview:pickerToolBar];
+    [popOverContent setView:popOverView];
+    
+    popOverController = [[UIPopoverController alloc]initWithContentViewController:popOverContent];
+    popOverController.popoverContentSize = CGSizeMake([picker frame].size.width, [popOverView frame].size.height);
+    [popOverController presentPopoverFromRect:[self button2].frame inView:[self view] permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    
+    
+    
+    
+}
+
+- (IBAction)button3:(id)sender {
+    _textField3.hidden = NO;
+    [_textField3 becomeFirstResponder];
+    
+    
+     
+    
+    
+
+}
+
+- (IBAction)textField3Done:(id)sender {
+    _textField3.hidden = YES;
+    [[self label3]setText:self.textField3.text];
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+    
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [arrayBuilder count];
+    
+}
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return [arrayBuilder objectAtIndex:row];
+    
+}
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    if (buttonIndex == 1){
+    [[self label1]setText:[arrayBuilder objectAtIndex:row]];
+    }
+    if (buttonIndex == 2) {
+        [[self label2]setText:[arrayBuilder objectAtIndex:row]];
+    }
+    
+}
+-(void)donePressed
+{
+    [popOverController dismissPopoverAnimated:YES];
+    
+}
+-(void)cancelPressed
+{
+    [[self label1]setText:nil];
+    [popOverController dismissPopoverAnimated:YES];
+
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [_textField3 resignFirstResponder];
+    return YES;
+    
+}
+
 
 @end
